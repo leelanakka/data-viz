1cbbb2006f2bac63216f2c633c9c649819385a4a
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2020-02-08 14:35:36 +0530

    first day

--- public/js/buildings.js
+++ public/js/buildings.js
@@ -1,8 +1,41 @@
-const drawBuildings = (buildings)=>{
+const drawBuildings = buildings => {
+  const width = 400;
+  const maxHeight = _.maxBy(buildings, building => building.height).height;
+  const height = 400;
+
   const toLine = b => `<strong>${b.name}</strong> <i>${b.height}</i>`;
-  document.querySelector('#chart-area').innerHTML = buildings.map(toLine).join('<hr/>');
-}
-const main = ()=>{
-  d3.json('data/buildings.json').then(drawBuildings);
-}
+  document.querySelector("#chart-area").innerHTML = buildings
+    .map(toLine)
+    .join("<hr/>");
+
+  const container = d3.select("#chart-data");
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, maxHeight])
+    .range([0, height]);
+
+  const x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(_.map(buildings, "name"))
+    .padding(0.3);
+
+  const svg = container
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  const rectangles = svg.selectAll("rect").data(buildings);
+
+  const newRects = rectangles
+    .enter()
+    .append("rect")
+    .attr("x", b => x(b.name))
+    .attr("width", x.bandwidth)
+    .attr("height", b => y(b.height));
+};
+const main = () => {
+  d3.json("data/buildings.json").then(drawBuildings);
+};
 window.onload = main;
