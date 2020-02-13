690e7cbc7dc80c1fa69e399cd507a2a4b8ff7fc0
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2020-02-13 18:06:02 +0530

    Adds transistion to all the companies and shows the all the fields in the chart

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,11 +1,10 @@
-const drawCompanies = companies => {
-  const chartSize = { width: 800, height: 600 };
-  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
-  const width = chartSize.width - margin.left - margin.right;
-  const maxHeight = _.maxBy(companies, "CMP").CMP;
-  const height = chartSize.height - margin.top - margin.bottom;
+const chartSize = { width: 800, height: 600 };
+const margin = { left: 100, right: 10, top: 20, bottom: 150 };
+const width = chartSize.width - margin.left - margin.right;
+const height = chartSize.height - margin.top - margin.bottom;
 
-  const container = d3.select("#chart-data");
+const drawCompanies = companies => {
+  const maxHeight = _.maxBy(companies, "CMP").CMP;
   const y = d3
     .scaleLinear()
     .domain([0, maxHeight])
@@ -16,15 +15,17 @@ const drawCompanies = companies => {
     .range([0, width])
     .domain(_.map(companies, "Name"))
     .padding(0.3);
-  const c = d3.scaleOrdinal(d3.schemeCategory10)
-  const svg = container
-    .append("svg")
+  const c = d3.scaleOrdinal(d3.schemeCategory10);
+
+  const svg = d3
+    .select("#chart-area svg")
     .attr("width", chartSize.width)
     .attr("height", chartSize.height);
 
   const g = svg
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
+
   g.append("text")
     .attr("class", "x axis-label")
     .attr("x", width / 2)
@@ -70,12 +71,59 @@ const drawCompanies = companies => {
     .attr("fill", b => c(b.Name));
 };
 
+const percentageFormat = d => `${d}%`;
+const kCroresFormat = d => `${d / 1000}k cr ₹`;
+
+const formats = {
+  MarketCap: kCroresFormat,
+  DivYld: percentageFormat,
+  ROCE: percentageFormat,
+  QNetProfit: kCroresFormat,
+  QSales: kCroresFormat
+};
+
+const updateCompanies = function(companies, fieldName) {
+  const svg = d3.select("#chart-area svg");
+  svg.select(".y.axis-label").text(fieldName);
+  const y = d3
+    .scaleLinear()
+    .domain([0, _.maxBy(companies, fieldName)[fieldName]])
+    .range([height, 0]);
+
+  const y_axis = d3
+    .axisLeft(y)
+    .tickFormat(formats[fieldName])
+    .ticks(8);
+
+  svg.select(".y-axis").call(y_axis);
+
+  const t = d3
+    .transition()
+    .duration(1000)
+    .ease(d3.easeLinear);
+
+  svg
+    .selectAll("rect")
+    .data(companies)
+    .transition(t)
+    .attr("y", c => y(c[fieldName]))
+    .attr("height", c => y(0) - y(c[fieldName]));
+};
+
 const parseCompany = ({ Name, ...numerics }) => {
   _.forEach(numerics, (v, k) => (numerics[k] = +v));
   return { Name, ...numerics };
 };
 
 const main = () => {
-  d3.csv("data/companies.csv", parseCompany).then(drawCompanies);
+  d3.csv("data/companies.csv", parseCompany).then(companies => {
+    drawCompanies(companies);
+    const fields = "CMP,PE,MarketCap,DivYld,QNetProfit,QSales,ROCE".split(",");
+    let step = 0;
+    setInterval(
+      () => updateCompanies(companies, fields[step++ % fields.length]),
+      2000
+    );
+  });
 };
 window.onload = main;
